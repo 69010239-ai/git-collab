99731434bbd0439c56fdbc4b592b1d4f586d084f
KMITL-69010236 <69010236@kmitl.ac.th>
2026-09-23 18:10:47 +0700

    Add Farewell

--- Hello.c
+++ Hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int farewell(const char *name)
+{
+printf("Goodbye, %s!\n", name);
+return 0;
+}
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
@@ -10,5 +15,7 @@ int main(void)
 {
     //print a greeting messafe for the specified user
     greeting("Developer A,B,C !!");
+    
+    farewell("Developer A,B,C");
     return 0;
 }
